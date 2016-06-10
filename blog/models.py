b6d90210d98072fbfccbb534559b67bf9d4ed289
Levent Altunoz <leventaltunoz@gmail.com>
2016-06-10 09:28:37 +0300

    isyerine gecis tag baslarken

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager,self).get_queryset().filter(status='published')
 class Post(models.Model):
 	STATUS_CHOICES = (
 	('draft', 'Draft'),
 	('published', 'Published'),
 	)
+	objects = models.Manager() # The default manager.
+	published = PublishedManager() # Our custom manager.
 	title = models.CharField(max_length=250)
 	slug = models.SlugField(max_length=250, unique_for_date='publish')
 	author = models.ForeignKey(User, related_name='blog_posts')
@@ -18,3 +25,6 @@ class Post(models.Model):
 		ordering = ('-publish',)
 	def __str__(self):
 		return self.title
+	def get_absolute_url(self):
+		return reverse('blog:post_detail', args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), 
+		self.slug])
